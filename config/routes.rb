8759b913bd7805b8affddea77fc01252584df69e
ahmetadibelli <ahmt.adibelli@hotmail.com>
2021-11-27 00:04:50 +0500

    route and controller added for comments

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,13 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   namespace :api, defaults: {format: :json} do
-    resources :users, only: [:create, :show]
+    resources :users, only: [:create, :show] do
+      resources :comments
+    end
     resource :session, only: [:create, :destroy, :show]
-    resources :articles
+    resources :articles do
+      resources :comments
+    end
   end
 
   root "static_pages#root"
