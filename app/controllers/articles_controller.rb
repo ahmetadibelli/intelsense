83c8e9d83c4ce2a14c1b49b7bb6f87ab76beae2d
ahmetadibelli <ahmt.adibelli@hotmail.com>
2021-11-27 00:13:22 +0500

    index action added for articles

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,5 +1,10 @@
 class ArticlesController < ApplicationController
   before_action :set_article, only: %i[update destroy]
+
+  def index
+    @articles = Article.all
+  end
+
   def create
     @article = Article.new(params[:article])
     if @article.save!
