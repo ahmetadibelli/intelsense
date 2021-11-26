70c1452f0eb87eb1aecc9936b19a254b6784b1b5
ahmetadibelli <ahmt.adibelli@hotmail.com>
2021-11-27 00:24:51 +0500

    index action added for article comments

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,2 +1,13 @@
 class CommentsController < ApplicationController
+  before_action :set_article, %i[index create]
+
+  def index
+    @comments = @article.comments
+  end
+
+  private
+
+  def set_article
+    @article = Article.find(params[:article_id])
+  end
 end
