ddf683c7bb98ce0bde1128eec5b2be8cf0722f7a
ahmetadibelli <ahmt.adibelli@hotmail.com>
2021-11-27 00:13:52 +0500

    show action added for article

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,8 +1,11 @@
 class ArticlesController < ApplicationController
-  before_action :set_article, only: %i[update destroy]
+  before_action :set_article, only: %i[show update destroy]
 
   def index
     @articles = Article.all
+  end
+
+  def show
   end
 
   def create
