7864fba00df41630c55fec036d9b1f10f31f40d2
ahmetadibelli <ahmt.adibelli@hotmail.com>
2021-11-29 00:55:51 +0500

    create action added for likes controller

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,8 +1,16 @@
 class LikesController < ApplicationController
-  before_action :set_article, %i[index]
+  before_action :set_article, %i[index create]
 
   def index
     @likes = @article.likes
+  end
+
+  def create
+    if @article.likes.create!(params[:like].merge(user_id: params[:user_id]))
+      { success: true, message: 'Article Liked Successfully' }
+    else
+      { success: false, message: 'Action Failed' }
+    end
   end
 
   private
