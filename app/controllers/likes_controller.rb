3d334e68cc43d5d2606ef30a2d883305f54b8280
ahmetadibelli <ahmt.adibelli@hotmail.com>
2021-11-29 00:57:19 +0500

    destroy action added for likes controller

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,5 +1,6 @@
 class LikesController < ApplicationController
   before_action :set_article, %i[index create]
+  before_action :set_like, %i[destroy]
 
   def index
     @likes = @article.likes
@@ -13,9 +14,21 @@ class LikesController < ApplicationController
     end
   end
 
+  def destroy
+    if @like.destroy
+      { success: true, message: 'Like Destroyed Successfully' }
+    else
+      { success: false, message: 'Action Failed' }
+    end
+  end
+
   private
 
   def set_article
     @article = Article.find(params[:article_id])
+  end
+
+  def set_like
+    @like = Like.find(params[:id])
   end
 end
