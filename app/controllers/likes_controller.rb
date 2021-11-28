82fe9e8a734e9064a52ab404302164065c585eb2
ahmetadibelli <ahmt.adibelli@hotmail.com>
2021-11-29 00:53:00 +0500

    index action added for likes controller

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,2 +1,13 @@
 class LikesController < ApplicationController
+  before_action :set_article, %i[index]
+
+  def index
+    @likes = @article.likes
+  end
+
+  private
+
+  def set_article
+    @article = Article.find(params[:article_id])
+  end
 end
