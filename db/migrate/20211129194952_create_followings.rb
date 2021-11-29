98083b91a1014eeb894b45bd72a8f7a7155d6859
ahmetadibelli <ahmt.adibelli@hotmail.com>
2021-11-30 00:58:39 +0500

    migration created for following join table

--- db/migrate/20211129194952_create_followings.rb
+++ db/migrate/20211129194952_create_followings.rb
@@ -1,6 +1,8 @@
 class CreateFollowings < ActiveRecord::Migration[5.2]
   def change
     create_table :followings do |t|
+      t.bigint :follower_id, index: true, foreign_key: true
+      t.bigint :followed_id, index: true, foreign_key: true
 
       t.timestamps
     end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_11_28_193933) do
+ActiveRecord::Schema.define(version: 2021_11_29_194952) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -32,6 +32,15 @@ ActiveRecord::Schema.define(version: 2021_11_28_193933) do
     t.datetime "updated_at", null: false
     t.index ["article_id"], name: "index_comments_on_article_id"
     t.index ["user_id"], name: "index_comments_on_user_id"
+  end
+
+  create_table "followings", force: :cascade do |t|
+    t.bigint "follower_id"
+    t.bigint "followed_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["followed_id"], name: "index_followings_on_followed_id"
+    t.index ["follower_id"], name: "index_followings_on_follower_id"
   end
 
   create_table "likes", force: :cascade do |t|
